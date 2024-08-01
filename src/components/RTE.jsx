0da32c832737037e62522a5276f29a0aa0240a06
AnmolGoyal01 <139466193+AnmolGoyal01@users.noreply.github.com>
2024-08-01 15:04:29 +0530

    Fix Tiny-Mce-api-key

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -12,7 +12,7 @@ export default function RTE({ name, control, label, defaultValue = "" }) {
         control={control}
         render={({ field: { onChange } }) => (
           <Editor
-            apiKey={String(import.meta.env.VITE_TINY_MCE_API_KEY)}
+            apiKey= "ujnmxvrs1qg89ux0ck858n4vjjdtmq4t977b1lob5lk3r3qq"
             initialValue={defaultValue}
             init={{
               initialValue: defaultValue,
